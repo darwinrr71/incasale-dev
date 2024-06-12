b7be7b95234577d9b7a499679f83e7007ab0acff
darwinrr71 <latin7106@gmail.com>
2024-06-13 00:11:41 +0200

    change the main

--- src/main.jsx
+++ src/main.jsx
@@ -13,10 +13,11 @@ import "@fontsource/roboto/700.css";
 //import "./index.css";
 //import { ThemeProvider } from "@emotion/react";
 //import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
+//import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
+HashRouter
 
 
 const theme = createTheme({
@@ -34,10 +35,10 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <HashRouter>
         <CssBaseline /> {/** Reinicia todos los estilos base y usa la fuente Roboto */}
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
